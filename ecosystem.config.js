f938b9249dc991e296e9cdfe5f2530108193f5fc
zhaoshichao <zhaosc@lenovocloud.com>
2020-08-06 15:45:26 +0800

    配置信息修改

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,25 @@
 module.exports = {
   apps : [{
+    name:'app',
     script: './bin/www',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
+    watch: '.',
+    instances: 0,
+    autorestart: true,
+    watch: true,
+    ignore_watch: [ // 不⽤监听的⽂件
+      "node_modules",
+      "logs"
+    ],
+    max_memory_restart: '1G',
+    "error_file": "./logs/app-err.log", // 错误⽇志⽂件
+    "out_file": "./logs/app-out.log",
+    "log_date_format": "YYYY-MM-DD HH:mm:ss", // 给每⾏⽇志标记⼀个时间
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
   }],
 
   deploy : {
